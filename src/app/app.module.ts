0c68b8c6cfdd3fed76bba91824452860d6ae33cb
Raul Mansur <raulfmansur@hotmail.com>
2018-05-01 03:02:30 -0300

    Scale generator functions added + inputs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { HomeModule } from './modules/home/home.module';
 
 
 @NgModule({
@@ -10,9 +10,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/modules/home/home.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+	MUSICALNOTES = ['A','A#','B','C','C#','D','D#','E','F','F#','G','G#']
+	SCALETYPES = ['Maior','Menor']
+	
+	scale = {key: '', type:'', notes: []}
+
+	constructor(){}
+	ngOnInit(){}
+
+	majorScale(key){
+		let notes = []
+		let start = this.MUSICALNOTES.findIndex(notes => notes===key)
+		// T T ST T T T ST
+		notes.push(this.MUSICALNOTES[start])
+		notes.push(this.MUSICALNOTES[(start+2)%12])
+		notes.push(this.MUSICALNOTES[(start+4)%12])
+		notes.push(this.MUSICALNOTES[(start+5)%12])
+		notes.push(this.MUSICALNOTES[(start+7)%12])
+		notes.push(this.MUSICALNOTES[(start+9)%12])
+		notes.push(this.MUSICALNOTES[(start+11)%12])
+		return notes
+	}
+
+	minorScale(key){
+		let notes = []
+		let start = this.MUSICALNOTES.findIndex(notes => notes===key)
+		// T ST T T ST T T
+		notes.push(this.MUSICALNOTES[start])
+		notes.push(this.MUSICALNOTES[(start+2)%12])
+		notes.push(this.MUSICALNOTES[(start+3)%12])
+		notes.push(this.MUSICALNOTES[(start+5)%12])
+		notes.push(this.MUSICALNOTES[(start+7)%12])
+		notes.push(this.MUSICALNOTES[(start+8)%12])
+		notes.push(this.MUSICALNOTES[(start+10)%12])
+		return notes
+	}
+
+	tryToCreate(value,menu){
+		this.scale[menu] = value
+
+		if(this.scale.key!='' && this.scale.type!=''){
+			if(this.scale.type=='Maior')
+				this.scale.notes = this.majorScale(this.scale.key)
+			else if(this.scale.type=='Menor')
+				this.scale.notes = this.minorScale(this.scale.key)				
+		}
+	}
+}
