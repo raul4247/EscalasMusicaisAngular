7cbcba0dac96bf4be5066e8345070db9c7517e3a
Raul Mansur <raulfmansur@hotmail.com>
2018-05-14 01:13:26 -0300

    minor guitar style changes

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -14,6 +14,24 @@ export class HomeComponent implements OnInit {
 	scale = {key: '', type:'', notes: []}
 
 	constructor(){
+		/*
+		var notes = {
+			e: ['E','F','F#','G','G#','A','A#','B','C','C#','D','D#','E'],
+			a: ['A','A#','B','C','C#','D','D#','E','F','F#','G','G#',"A"],
+			d: ['D','D#','E','F','F#','G','G#','A','A#','B','C','C#','D'],
+			g: ['G','G#','A','A#','B','C','C#','D','D#','E','F','F#','G'],
+			b: ['B','C','C#','D','D#','E','F','F#','G','G#','A','A#','B']
+		}
+
+		for (var i=0; i < notes.e.length; i++){
+			$('.mask.low-e ul').append('<li note='+notes.e[i]+'>'+notes.e[i]+'</li>')
+			$('.mask.a ul').append('<li note='+notes.a[i]+'>'+notes.a[i]+'</li>')
+			$('.mask.d ul').append('<li note='+notes.d[i]+'>'+notes.d[i]+'</li>')
+			$('.mask.g ul').append('<li note='+notes.g[i]+'>'+notes.g[i]+'</li>')
+			$('.mask.b ul').append('<li note='+notes.b[i]+'>'+notes.b[i]+'</li>')
+			$('.mask.high-e ul').append('<li note='+notes.e[i]+'>'+notes.e[i]+'</li>')
+		}
+		*/
 	}
 	ngOnInit(){}
 
